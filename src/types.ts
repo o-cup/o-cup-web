d900b06e29fecd79cb01f8dadacf6458a21cee0b
jheeju <jheeju.dev@gmail.com>
2022-07-06 22:09:13 +0900

    feat(9): EventList 컴포넌트에 supabase 데이터 연동

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type Event = {
+	id: number;
+	createdAt: string;
+	place: string;
+	bias: string[];
+	organizer: string;
+	snsId: string;
+	district: string;
+	startAt: string;
+	endAt: string;
+	images: string[];
+};

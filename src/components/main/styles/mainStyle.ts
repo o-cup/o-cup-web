874c607b7901219fa8ef74b01948e9a95c74f71c
jheeju <jheeju.dev@gmail.com>
2022-08-14 17:12:58 +0900

    feat(develop): 메인 이미지 세로 크기 270px으로 확장

--- src/components/main/styles/mainStyle.ts
+++ src/components/main/styles/mainStyle.ts
@@ -165,10 +165,9 @@ const StyledItem = styled.li`
 
 		> img {
 			width: 100%;
-			height: 230px;
+			height: 270px;
 			object-fit: cover;
 			object-position: top;
-			//border: 2px solid #000000;
 		}
 
 		> h6 {

565a4a47b5adbbcbb9c31f95cb2f1dcbcc5ffd75
jheeju <jheeju.dev@gmail.com>
2022-08-20 11:04:01 +0900

    feat(68): 월 선택 컴포넌트 구현

--- src/components/search/MonthSelector.tsx
+++ src/components/search/MonthSelector.tsx
@@ -1,16 +1,36 @@
-import React from "react";
-import ToggleButton from "../../shared/components/ToggleButton";
-import { StyledMonthSelector } from "./styles/monthSelectorStyle";
+import React, { useState } from "react";
+import { FaCaretDown, FaCaretUp } from "react-icons/fa";
+import { Month, StyledMonthSelector } from "./styles/monthSelectorStyle";
 
-const MonthSelector = () => (
-	<StyledMonthSelector>
-		<h2>8월 생일 아티스트를 찾아보세요.</h2>
+const MonthSelector = () => {
+	const [toggle, setToggle] = useState(false);
+	const [selectedMonth, setSelectedMonth] = useState<number>(8);
 
-		<div className="toggle">
-			<p>다른 생일 달 아티스트 찾아보기</p>
-			<ToggleButton width={12} />
-		</div>
-	</StyledMonthSelector>
-);
+	const handleToggleClick = () => setToggle(!toggle);
+
+	const months = Array.from({ length: 12 }, (_, i) => i + 1);
+
+	return (
+		<StyledMonthSelector>
+			<h2>8월 생일 아티스트를 찾아보세요.</h2>
+			<div className="toggle">
+				<p>다른 생일 달 아티스트 찾아보기</p>
+				{toggle ? <FaCaretUp onClick={handleToggleClick} /> : <FaCaretDown onClick={handleToggleClick} />}
+			</div>
+
+			{toggle && (
+				<div className="selectBox">
+					{months.map((month) => (
+						<Month
+							key={month}
+							isActive={selectedMonth === month}
+							onClick={() => setSelectedMonth(month)}
+						>{`${month}월`}</Month>
+					))}
+				</div>
+			)}
+		</StyledMonthSelector>
+	);
+};
 
 export default MonthSelector;
--- src/components/search/styles/monthSelectorStyle.ts
+++ src/components/search/styles/monthSelectorStyle.ts
@@ -11,6 +11,7 @@ export const StyledMonthSelector = styled.div`
 
 	padding: 10px;
 	background-color: ${({ theme }) => theme.colors.white};
+	position: relative;
 
 	h2 {
 		font-size: 18px;
@@ -20,6 +21,7 @@ export const StyledMonthSelector = styled.div`
 
 	.toggle {
 		display: flex;
+		align-items: center;
 		gap: 5px;
 
 		p {
@@ -28,6 +30,35 @@ export const StyledMonthSelector = styled.div`
 			width: fit-content;
 		}
 	}
+
+	.selectBox {
+		width: 380px;
+		height: 96px;
+		border: 2px solid #000;
+		position: absolute;
+		bottom: -102px;
+		left: -2px;
+		box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
+		display: flex;
+		justify-content: space-around;
+		gap: 10px;
+		flex-wrap: wrap;
+		padding: 10px;
+	}
+`;
+
+export const Month = styled.span<{ isActive: boolean }>`
+	width: 46px;
+	height: 30px;
+	font-size: 12px;
+	font-weight: bold;
+	text-align: center;
+	border-radius: 4px;
+
+	background-color: ${({ isActive, theme }) => (isActive ? theme.colors.primary : theme.colors.black)};
+	color: ${({ isActive, theme }) => (isActive ? theme.colors.black : theme.colors.white)};
+	border: ${({ isActive }) => (isActive ? "2px solid #000" : "")};
+	line-height: ${({ isActive }) => (isActive ? "28px" : "30px")};
 `;
 
 export default {};

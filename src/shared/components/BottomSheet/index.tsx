0f23f0c8246879af75584a6b0fb5f9aa8ada9ffb
jheeju <jheeju.dev@gmail.com>
2022-08-22 22:01:48 +0900

    fix(69): merge conflicts

--- src/shared/components/BottomSheet/index.tsx
+++ src/shared/components/BottomSheet/index.tsx
@@ -6,61 +6,71 @@ import "./react-spring-bottom-sheet.css";
 import Icon from "../Icon/Icons";
 
 type ButtonContent = {
-  title: string;
-  handleClick?: (e: React.FormEvent<HTMLButtonElement>) => void;
-}
+	title: string;
+	handleClick?: (e: React.FormEvent<HTMLButtonElement>) => void;
+};
 
 type BottomSheetProps = {
-  open: boolean;
-  setOpen: Dispatch<SetStateAction<boolean>>;
-  header?: string;
-  close?: boolean;
-  slider?: boolean;
-  buttons?: ButtonContent;
-  children: JSX.Element;
-}
+	open: boolean;
+	setOpen: Dispatch<SetStateAction<boolean>>;
+	header?: string;
+	close?: boolean;
+	slider?: boolean;
+	buttons?: ButtonContent;
+	children: JSX.Element;
+};
 
 export default function BottomSheet({ open, setOpen, header, close, slider, buttons, children }: BottomSheetProps) {
+	function onDismiss() {
+		setOpen(false);
+		return false;
+	}
 
-  function onDismiss() {
-    setOpen(false);
-    return false;
-  }
-
-  return (
-    <RBottomSheet open={open}
-                  onDismiss={() => onDismiss()}
-                  header={
-                    <div className={`bottom-header ${slider ? "slider" : ""}`}>
-                      {header && <p className="title">{header}</p>}
-                      {close && <button type="button" className="close" onClick={() => {
-                        setOpen(false);
-                      }}>
-                        <Icon name="delete-circle-black"/>
-                      </button>}
-                    </div>
-                  }
-
-                  footer={
-                    <div className="bottom-footer">
-                      {buttons ? <button type="button" onClick={buttons.handleClick}>{buttons.title}</button> : ""}
-                    </div>
-                  }
-
-                  initialFocusRef={false}
-                  snapPoints={({ headerHeight, minHeight }) => [headerHeight + minHeight]}
-    >
-      {children}
-    </RBottomSheet>
-  );
+	return (
+		<RBottomSheet
+			open={open}
+			onDismiss={() => onDismiss()}
+			header={
+				<div className={`bottom-header ${slider ? "slider" : ""}`}>
+					{header && <p className="title">{header}</p>}
+					{close && (
+						<button
+							type="button"
+							className="close"
+							onClick={() => {
+								setOpen(false);
+							}}
+						>
+							<Icon name="delete-circle-black" />
+						</button>
+					)}
+				</div>
+			}
+			footer={
+				<div className="bottom-footer">
+					{buttons ? (
+						<button type="button" onClick={buttons.handleClick}>
+							{buttons.title}
+						</button>
+					) : (
+						""
+					)}
+				</div>
+			}
+			initialFocusRef={false}
+			snapPoints={({ headerHeight, minHeight }) => [headerHeight + minHeight]}
+		>
+			{children}
+		</RBottomSheet>
+	);
 }
 
 BottomSheet.defaultProps = {
-  header: "",
-  close: false,
-  slider: false,
-  buttons: {
-    title: "",
-    handleClick: () => console.log("click!"),
-  },
+	header: "",
+	close: false,
+	slider: false,
+	buttons: {
+		title: "",
+		handleClick: () => console.log("click!"),
+	},
 };

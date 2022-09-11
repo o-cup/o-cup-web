e58035206ca07491a074cb2012c3d8ee758b352c
jheeju <jheeju.dev@gmail.com>
2022-09-11 23:51:57 +0900

    feat(221): 트래킹 아이디 읽지 못하는 문제 테스트

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ReactGA from "react-ga";
 import App from "./App";
-
-const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
-ReactGA.initialize(TRACKING_ID!);
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(

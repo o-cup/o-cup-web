11e2d6a12e209aaded40b63c5d7a38b745bdfcda
hjnk125 <60878616+hjnk125@users.noreply.github.com>
2022-09-21 16:37:15 +0900

    feat(251): 포스터 슬라이더 배경색 변경

--- src/components/detail/styles/posterViewStyle.ts
+++ src/components/detail/styles/posterViewStyle.ts
@@ -8,7 +8,7 @@ export const StyledPosterView = styled.div`
 	top: 0;
 	width: 100%;
 	height: 100vh;
-	background-color: rgba(0, 0, 0, 0.4);
+	background-color: rgba(0, 0, 0, 0.6);
 	overflow: hidden;
 
 	i {

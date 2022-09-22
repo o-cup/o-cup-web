88aaeeb5e4a73f3a48ec2f59037619086dd4c912
hjnk125 <60878616+hjnk125@users.noreply.github.com>
2022-09-22 21:53:39 +0900

    feat(254): 가까운 연관 이벤트 명칭 변경, Detail.tsx props 리팩토링

--- src/components/request/PreviewContent.tsx
+++ src/components/request/PreviewContent.tsx
@@ -9,25 +9,31 @@ const PreviewContent = () => {
 	const requestInputs = useRecoilValue(requestInputsAtom);
 	const goodsList = useRecoilValue(requestGoodsListAtom);
 
-	const { place, artist, organizer, snsId, link, posterUrls, hashTags, dateRange, goods } = requestInputs;
+	const { place, artist, organizer, snsId, link, posterUrls, hashTags, dateRange } = requestInputs;
 
 	return (
 		<div className="previewContent">
 			<EventMain
-				place={place.place || "카페이름"}
-				biasesId={[]}
-				requestedBiases={artist[0].bias ? artist : [{ id: 1, peopleId: 0, bias: "아티스트 이름", team: "" }]}
-				organizer={organizer || "주최자 닉네임"}
-				snsId={snsId || "ocup_official"}
-				startAt={dateRange.startAt || "20220000"}
-				endAt={dateRange.endAt || "20220000"}
-				address={place.address || "이벤트 주소"}
-				images={posterUrls[0].publicUrl ? posterUrls.filter((poster) => poster.publicUrl !== "").map((poster) => poster.publicUrl) : [DEFAULT_POSTER_URL]}
+				data={{
+					place: place.place || "카페이름",
+					biasesId: [],
+					requestedBiases: artist[0].bias ? artist : [{ id: 1, peopleId: 0, bias: "아티스트 이름", team: "" }],
+					organizer: organizer || "주최자 닉네임",
+					snsId: snsId || "ocup_official",
+					startAt: dateRange.startAt || "20220000",
+					endAt: dateRange.endAt || "20220000",
+					address: place.address || "이벤트 주소",
+					images: posterUrls[0].publicUrl
+						? posterUrls.filter((poster) => poster.publicUrl !== "").map((poster) => poster.publicUrl)
+						: [DEFAULT_POSTER_URL],
+				}}
 			/>
 			<TwitterInfo
-				organizer={organizer || "주최자 닉네임"}
-				snsId={snsId || "ocup_official"}
-				hashTags={hashTags[0].text ? hashTags.map((h) => h.text) : ["해피_오컵_데이"]}
+				data={{
+					organizer: organizer || "주최자 닉네임",
+					snsId: snsId || "ocup_official",
+					hashTags: hashTags[0].text ? hashTags.map((h) => h.text) : [""],
+				}}
 			/>
 			<GoodsInfo goods={getGoodsObj(requestInputs, goodsList)} tweetUrl={link} />
 			<Location address={place.address} />

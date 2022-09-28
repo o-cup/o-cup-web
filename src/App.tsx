780efbaae00f5d9fa081ed36d61bf61d3be08b48
hjnk125 <60878616+hjnk125@users.noreply.github.com>
2022-09-28 17:29:31 +0900

    fix(260): 100vh 수정

--- src/App.tsx
+++ src/App.tsx
@@ -26,10 +26,12 @@ function App() {
 		ReactGA.initialize(TRACKING_ID!);
 	}, [TRACKING_ID]);
 
-	useEffect(() => {
+	function setScreenSize() {
 		const vh = window.innerHeight * 0.01;
 		document.documentElement.style.setProperty("--vh", `${vh}px`);
-	});
+	}
+
+	window.addEventListener("resize", () => setScreenSize());
 
 	return (
 		<QueryClientProvider client={queryClient}>

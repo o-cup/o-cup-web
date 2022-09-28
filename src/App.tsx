792d1e4ea9c3f2e749ef9bb4789b8619282d7eea
hjnk125 <60878616+hjnk125@users.noreply.github.com>
2022-09-28 17:24:04 +0900

    fix(260): 100vh 다시수정...

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
 	useEffect(() => {
 		const vh = window.innerHeight * 0.01;
 		document.documentElement.style.setProperty("--vh", `${vh}px`);
-	}, []);
+	});
 
 	return (
 		<QueryClientProvider client={queryClient}>

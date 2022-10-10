c34861b201ea6851df571cb8f8a252fba98a76e6
hjnk125 <60878616+hjnk125@users.noreply.github.com>
2022-10-10 17:27:09 +0900

    feat(331): 등록 미리보기 카테고리 추가

--- src/components/request/PreviewContent.tsx
+++ src/components/request/PreviewContent.tsx
@@ -10,13 +10,14 @@ const PreviewContent = () => {
 	const goodsList = useRecoilValue(requestGoodsListAtom);
 	const tempPosters = useRecoilValue(tempPostersAtom);
 
-	const { place, artist, organizer, snsId, link, hashTags, dateRange } = requestInputs;
+	const { place, category, artist, organizer, snsId, link, hashTags, dateRange } = requestInputs;
 
 	return (
 		<div className="previewContent">
 			<DetailMainInfo
 				data={{
 					place: place.place || "카페이름",
+					category: category || "A",
 					biasesId: [],
 					requestedBiases: artist[0].bias ? artist : [{ id: 1, peopleId: 0, bias: "아티스트 이름", team: "" }],
 					organizer: organizer || "주최자 닉네임",

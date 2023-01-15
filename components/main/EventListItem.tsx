64d6920e1ba1aad624d64f126bd5a7b323675dac
jheeju <jheeju.dev@gmail.com>
2023-01-15 22:40:59 +0900

    feat(421): 이벤트 라우팅 Link로 변경

--- components/main/EventListItem.tsx
+++ components/main/EventListItem.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+import Link from "next/link";
 import React from "react";
 import Icon from "../../shared/components/icon";
 import { convertDateWithDots, imageOnErrorHandler } from "../../shared/utils";
@@ -13,39 +13,39 @@ type EventListItemProps = {
 };
 
 const EventListItem = ({ event }: EventListItemProps) => {
-	const router = useRouter();
-
 	const { id, place, image, category, snsId, districts, startAt, endAt } =
 		event;
 
 	return (
-		<StyledMainListItem onClick={() => router.push(`/detail/${id}`)}>
-			<StyledCategoryBorder type={category} />
-			<div className="imgContainer">
-				<img alt={image} src={image} onError={imageOnErrorHandler} />
-			</div>
-			<div className="title">
-				<p>{place}</p>
-				{/* <BiasChip id={biasesId[0]} key={biasesId[0]} dots={biasesId.length > 1} /> */}
-			</div>
-			<ul className="textContainer">
-				<li>
-					<Icon name="host-gray" />
-					<p>{snsId ? `@${snsId}` : "-"}</p>
-				</li>
-				<li>
-					<Icon name="place-gray" />
-					<p>{districts.name}</p>
-				</li>
-				<li>
-					<Icon name="calendar-gray" />
-					<p>
-						{startAt && convertDateWithDots(startAt)} -{" "}
-						{endAt && convertDateWithDots(endAt)}
-					</p>
-				</li>
-			</ul>
-		</StyledMainListItem>
+		<Link href={`/detail/${id}`}>
+			<StyledMainListItem>
+				<StyledCategoryBorder type={category} />
+				<div className="imgContainer">
+					<img alt={image} src={image} onError={imageOnErrorHandler} />
+				</div>
+				<div className="title">
+					<p>{place}</p>
+					{/* <BiasChip id={biasesId[0]} key={biasesId[0]} dots={biasesId.length > 1} /> */}
+				</div>
+				<ul className="textContainer">
+					<li>
+						<Icon name="host-gray" />
+						<p>{snsId ? `@${snsId}` : "-"}</p>
+					</li>
+					<li>
+						<Icon name="place-gray" />
+						<p>{districts.name}</p>
+					</li>
+					<li>
+						<Icon name="calendar-gray" />
+						<p>
+							{startAt && convertDateWithDots(startAt)} -{" "}
+							{endAt && convertDateWithDots(endAt)}
+						</p>
+					</li>
+				</ul>
+			</StyledMainListItem>
+		</Link>
 	);
 };
 

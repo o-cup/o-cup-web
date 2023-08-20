5c63368347d2ef6fa4060e550b1b4a3914e0fc4b
heeju.jeon <heeju.jeon@musinsa.com>
2023-08-20 21:02:17 +0900

    feat(develop): 검색 결과 페이지 애드핏 추가

--- components/search/Result.tsx
+++ components/search/Result.tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import React, { memo, useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { Button, Chip, Icon, Loading, SortIcon } from "../../shared/components";
+import KakaoAdFit from "../../shared/components/kakaoAdFit";
 import { initialCategoryData } from "../../shared/constants";
 import { searchFiltersAtom } from "../../shared/state";
 import { getDateRangeText } from "../../shared/utils/dateHandlers";
@@ -13,181 +14,190 @@ import type { ResultSortOptionKeys } from "../../shared/types";
 import type { TempSearchFiltersType } from "./types";
 
 const Result = () => {
-	const router = useRouter();
-	const [searchFilters, setSearchFilters] = useRecoilState(searchFiltersAtom);
-	const {
-		date: { startDate, endDate },
-		districts,
-		categories,
-	} = searchFilters;
-	const [sortOpen, setSortOpen] = useState(false);
-	const [filterOpen, setFilterOpen] = useState(false);
-	const [sortOption, setSortOption] =
-		useState<ResultSortOptionKeys>("alphabetAsc");
-	const [isFilterOpen, setIsFilterOpen] = useState(false);
-	const [tempSearchFilters, setTempSearchFilters] =
-		useState<TempSearchFiltersType>({
-			date: {
-				startDate: null,
-				endDate: null,
-			},
-			districts: [],
-			categories: initialCategoryData,
-		});
+    const router = useRouter();
+    const [searchFilters, setSearchFilters] = useRecoilState(searchFiltersAtom);
+    const {
+        date: { startDate, endDate },
+        districts,
+        categories,
+    } = searchFilters;
+    const [sortOpen, setSortOpen] = useState(false);
+    const [filterOpen, setFilterOpen] = useState(false);
+    const [sortOption, setSortOption] =
+        useState<ResultSortOptionKeys>("alphabetAsc");
+    const [isFilterOpen, setIsFilterOpen] = useState(false);
+    const [tempSearchFilters, setTempSearchFilters] =
+        useState<TempSearchFiltersType>({
+            date: {
+                startDate: null,
+                endDate: null,
+            },
+            districts: [],
+            categories: initialCategoryData,
+        });
 
-	const { isLoading, events, endedEvents } = useSearchResult({
-		sortOption,
-	});
+    const { isLoading, events, endedEvents } = useSearchResult({
+        sortOption,
+    });
 
-	useEffect(() => {
-		if (sortOpen) {
-			setFilterOpen(false);
-		}
-	}, [sortOpen]);
+    useEffect(() => {
+        if (sortOpen) {
+            setFilterOpen(false);
+        }
+    }, [sortOpen]);
 
-	useEffect(() => {
-		if (filterOpen) {
-			setSortOpen(false);
-		}
-	}, [filterOpen]);
+    useEffect(() => {
+        if (filterOpen) {
+            setSortOpen(false);
+        }
+    }, [filterOpen]);
 
-	const handleFilterIconClick = () => {
-		setIsFilterOpen(true);
-		setTempSearchFilters(searchFilters);
-	};
+    const handleFilterIconClick = () => {
+        setIsFilterOpen(true);
+        setTempSearchFilters(searchFilters);
+    };
 
-	const handleDeleteChip = ({
-		type,
-		code,
-	}: {
-		type: "date" | "district" | "category";
-		code?: string;
-	}) => {
-		const newDistrictData = districts.filter((d) => d.code !== code);
-		const newCategoryData = categories.map((c) => ({
-			...c,
-			selected: c.code === code ? false : c.selected,
-		}));
+    const handleDeleteChip = ({
+        type,
+        code,
+    }: {
+        type: "date" | "district" | "category";
+        code?: string;
+    }) => {
+        const newDistrictData = districts.filter((d) => d.code !== code);
+        const newCategoryData = categories.map((c) => ({
+            ...c,
+            selected: c.code === code ? false : c.selected,
+        }));
 
-		switch (type) {
-			case "date":
-				setSearchFilters((prev) => ({
-					...prev,
-					date: { startDate: null, endDate: null },
-				}));
-				break;
+        switch (type) {
+            case "date":
+                setSearchFilters((prev) => ({
+                    ...prev,
+                    date: { startDate: null, endDate: null },
+                }));
+                break;
 
-			case "district":
-				setSearchFilters((prev) => ({ ...prev, districts: newDistrictData }));
+            case "district":
+                setSearchFilters((prev) => ({
+                    ...prev,
+                    districts: newDistrictData,
+                }));
 
-				break;
+                break;
 
-			case "category":
-				setSearchFilters((prev) => ({
-					...prev,
-					categories: newCategoryData,
-				}));
-				break;
+            case "category":
+                setSearchFilters((prev) => ({
+                    ...prev,
+                    categories: newCategoryData,
+                }));
+                break;
 
-			default:
-				break;
-		}
-	};
+            default:
+                break;
+        }
+    };
 
-	if (isLoading) {
-		return <Loading />;
-	}
+    if (isLoading) {
+        return <Loading />;
+    }
 
-	const selectedCategories = categories.filter((c) => c.selected);
+    const selectedCategories = categories.filter((c) => c.selected);
 
-	return (
-		<StyledResult>
-			<div className="menu">
-				<p>{`검색 결과 총 ${events?.length || 0}개`}</p>
-				<div className="icons">
-					<Icon name="filter" handleClick={handleFilterIconClick} />
-					<SortIcon
-						type="result"
-						isOpened={sortOpen}
-						setIsOpened={setSortOpen}
-						setSelectedResultOption={setSortOption}
-						selectedOption={sortOption}
-					/>
-				</div>
-			</div>
+    return (
+        <StyledResult>
+            <div className="menu">
+                <p>{`검색 결과 총 ${events?.length || 0}개`}</p>
+                <div className="icons">
+                    <Icon name="filter" handleClick={handleFilterIconClick} />
+                    <SortIcon
+                        type="result"
+                        isOpened={sortOpen}
+                        setIsOpened={setSortOpen}
+                        setSelectedResultOption={setSortOption}
+                        selectedOption={sortOption}
+                    />
+                </div>
+            </div>
 
-			<div className="chips">
-				{startDate && endDate && (
-					<Chip
-						key="date"
-						text={getDateRangeText(startDate, endDate)}
-						handleDelete={() =>
-							handleDeleteChip({
-								type: "date",
-							})
-						}
-						bgColor="white"
-					/>
-				)}
+            <div className="chips">
+                {startDate && endDate && (
+                    <Chip
+                        key="date"
+                        text={getDateRangeText(startDate, endDate)}
+                        handleDelete={() =>
+                            handleDeleteChip({
+                                type: "date",
+                            })
+                        }
+                        bgColor="white"
+                    />
+                )}
 
-				{districts?.map((district) => (
-					<Chip
-						key={district.code}
-						text={district.name}
-						handleDelete={() =>
-							handleDeleteChip({
-								type: "district",
-								code: district.code,
-							})
-						}
-						bgColor="white"
-					/>
-				))}
+                {districts?.map((district) => (
+                    <Chip
+                        key={district.code}
+                        text={district.name}
+                        handleDelete={() =>
+                            handleDeleteChip({
+                                type: "district",
+                                code: district.code,
+                            })
+                        }
+                        bgColor="white"
+                    />
+                ))}
 
-				{selectedCategories?.map((category) => (
-					<Chip
-						key={category.code}
-						text={category.name}
-						handleDelete={() =>
-							handleDeleteChip({
-								type: "category",
-								code: category.code,
-							})
-						}
-						bgColor="white"
-					/>
-				))}
-			</div>
+                {selectedCategories?.map((category) => (
+                    <Chip
+                        key={category.code}
+                        text={category.name}
+                        handleDelete={() =>
+                            handleDeleteChip({
+                                type: "category",
+                                code: category.code,
+                            })
+                        }
+                        bgColor="white"
+                    />
+                ))}
+            </div>
 
-			<ul className="events">
-				{[...events, ...endedEvents]?.map((event) => (
-					<ResultEventItem key={event.id} event={event} />
-				))}
-			</ul>
+            <ul className="events">
+                {[...events, ...endedEvents]?.map((event) => (
+                    <ResultEventItem key={event.id} event={event} />
+                ))}
+            </ul>
 
-			<div className="request">
-				<p>찾고 있는 이벤트가 없나요?</p>
-				<img src="/images/pin_cone.png" alt="request" />
-				<Button
-					customStyle={{ fontWeight: "bold", width: "178px", height: "50px" }}
-					handleClick={() => router.push("/request")}
-				>
-					이벤트 등록하기
-				</Button>
-			</div>
+            <KakaoAdFit unitCode="DAN-YheU4n7t9DC7Wloh" />
 
-			<FilterBottomSheet
-				isOpen={isFilterOpen}
-				setIsOpen={setIsFilterOpen}
-				tempSearchFilters={tempSearchFilters}
-				setTempSearchFilters={setTempSearchFilters}
-			/>
-		</StyledResult>
-	);
+            <div className="request">
+                <p>찾고 있는 이벤트가 없나요?</p>
+                <img src="/images/pin_cone.png" alt="request" />
+                <Button
+                    customStyle={{
+                        fontWeight: "bold",
+                        width: "178px",
+                        height: "50px",
+                    }}
+                    handleClick={() => router.push("/request")}
+                >
+                    이벤트 등록하기
+                </Button>
+            </div>
+
+            <FilterBottomSheet
+                isOpen={isFilterOpen}
+                setIsOpen={setIsFilterOpen}
+                tempSearchFilters={tempSearchFilters}
+                setTempSearchFilters={setTempSearchFilters}
+            />
+        </StyledResult>
+    );
 };
 
 Result.defaultProps = {
-	biasId: null,
+    biasId: null,
 };
 
 export default memo(Result);
--- components/search/styles/resultStyle.ts
+++ components/search/styles/resultStyle.ts
@@ -1,73 +1,72 @@
 import styled from "styled-components";
 
 export const StyledResult = styled.div`
-	width: 100%;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
 
-	.menu {
-		display: flex;
-		justify-content: space-between;
-		height: 24px;
-		margin-bottom: 10px;
+    .menu {
+        display: flex;
+        justify-content: space-between;
+        height: 24px;
+        margin-bottom: 10px;
 
-		p {
-			line-height: 24px;
-			font-size: 14px;
-			color: ${({ theme }) => theme.colors.gray};
-			width: 100%;
-		}
+        p {
+            line-height: 24px;
+            font-size: 14px;
+            color: ${({ theme }) => theme.colors.gray};
+            width: 100%;
+        }
 
-		.icons {
-			display: flex;
-			width: fit-content;
-			justify-content: flex-end;
-			align-items: center;
-			gap: 10px;
+        .icons {
+            display: flex;
+            width: fit-content;
+            justify-content: flex-end;
+            align-items: center;
+            gap: 10px;
 
-			i {
-				min-width: 24px;
-				max-width: 24px;
-			}
-		}
-	}
+            i {
+                min-width: 24px;
+                max-width: 24px;
+            }
+        }
+    }
 
-	.chips {
-		display: flex;
-		gap: 10px;
-		margin-bottom: 8px;
-		overflow-x: scroll;
+    .chips {
+        display: flex;
+        gap: 10px;
+        margin-bottom: 8px;
+        overflow-x: scroll;
 
-		::-webkit-scrollbar {
-			display: none;
-		}
-	}
+        ::-webkit-scrollbar {
+            display: none;
+        }
+    }
 
-	.events {
-		display: flex;
-		gap: 16px;
-		flex-wrap: wrap;
-	}
+    .events {
+        display: flex;
+        gap: 16px;
+        flex-wrap: wrap;
+        margin-bottom: 20px;
+    }
 
-	.request {
-		margin-top: 20px;
-		height: 240px;
+    .request {
+        height: 240px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
 
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: space-evenly;
+        img {
+            width: 56px;
+        }
 
-		img {
-			width: 56px;
-		}
-
-		p {
-			text-align: center;
-			line-height: 30px;
-		}
-	}
+        p {
+            text-align: center;
+            line-height: 30px;
+        }
+    }
 `;
 
 export default {};

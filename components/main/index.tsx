c15fa361eb9a8db625d8e96ac79b8101d42e4526
heeju.jeon <heeju.jeon@musinsa.com>
2023-08-20 21:21:06 +0900

    feat: 메인 쿼리파라미터 추가

--- components/main/index.tsx
+++ components/main/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { useQuery } from "react-query";
 import { useRecoilState } from "recoil";
@@ -13,58 +14,70 @@ import { fetchEventsByDate, getBiasListData } from "./fetchers";
 import { StyledMain } from "./styles/mainStyle";
 
 const Main = () => {
-	const [dateFilter, setDateFilter] = useRecoilState(dateFilterAtom);
-	const isToday = dateFilter === convertDateToString(new Date());
-	const monthIndex = convertStringToDate(dateFilter).getMonth();
-	const date = convertStringToDate(dateFilter).getDate();
+    const [dateFilter, setDateFilter] = useRecoilState(dateFilterAtom);
+    const isToday = dateFilter === convertDateToString(new Date());
+    const monthIndex = convertStringToDate(dateFilter).getMonth();
+    const date = convertStringToDate(dateFilter).getDate();
+    const router = useRouter();
+    const { pathname } = router;
 
-	useClearData();
+    useClearData();
 
-	const { refetch: refetchBiasList } = useQuery(["biasListByDate"], () =>
-		getBiasListData(dateFilter)
-	);
+    const { refetch: refetchBiasList } = useQuery(["biasListByDate"], () =>
+        getBiasListData(dateFilter)
+    );
 
-	const { refetch: refetchEventsList } = useQuery(["eventListByDate"], () =>
-		fetchEventsByDate(dateFilter)
-	);
+    const { refetch: refetchEventsList } = useQuery(["eventListByDate"], () =>
+        fetchEventsByDate(dateFilter)
+    );
 
-	useEffect(() => {
-		refetchBiasList();
-		refetchEventsList();
-	}, [dateFilter]);
+    useEffect(() => {
+        refetchBiasList();
+        refetchEventsList();
+    }, [dateFilter]);
 
-	const handleChangeDate = (type: "prev" | "next") => {
-		const selectedDate = convertStringToDate(dateFilter);
-		const result = new Date(selectedDate);
-		if (type === "prev") {
-			result.setDate(result.getDate() - 1);
-		}
-		if (type === "next") {
-			result.setDate(result.getDate() + 1);
-		}
-		setDateFilter(convertDateToString(result));
-	};
+    useEffect(() => {
+        router.push({
+            pathname,
+            query: { date: dateFilter },
+        });
+    }, [dateFilter]);
 
-	return (
-		<Layout page="main">
-			<StyledMain>
-				<div className="dateTitle">
-					<Icons
-						name="left-arrow-circle"
-						handleClick={() => handleChangeDate("prev")}
-					/>
-					<p>{isToday ? "오늘" : `${monthIndex + 1}월 ${date}일`}의 이벤트</p>
-					<Icons
-						name="right-arrow-circle"
-						handleClick={() => handleChangeDate("next")}
-					/>
-				</div>
-				<BiasList />
-				<KakaoAdFit unitCode="DAN-eloyaCOUNzkG0TLf" />
-				<EventSection />
-			</StyledMain>
-		</Layout>
-	);
+    const handleChangeDate = (type: "prev" | "next") => {
+        const selectedDate = convertStringToDate(dateFilter);
+        const result = new Date(selectedDate);
+        if (type === "prev") {
+            result.setDate(result.getDate() - 1);
+        }
+        if (type === "next") {
+            result.setDate(result.getDate() + 1);
+        }
+        setDateFilter(convertDateToString(result));
+    };
+
+    return (
+        <Layout page="main">
+            <StyledMain>
+                <div className="dateTitle">
+                    <Icons
+                        name="left-arrow-circle"
+                        handleClick={() => handleChangeDate("prev")}
+                    />
+                    <p>
+                        {isToday ? "오늘" : `${monthIndex + 1}월 ${date}일`}의
+                        이벤트
+                    </p>
+                    <Icons
+                        name="right-arrow-circle"
+                        handleClick={() => handleChangeDate("next")}
+                    />
+                </div>
+                <BiasList />
+                <KakaoAdFit unitCode="DAN-eloyaCOUNzkG0TLf" />
+                <EventSection />
+            </StyledMain>
+        </Layout>
+    );
 };
 
 export default Main;
